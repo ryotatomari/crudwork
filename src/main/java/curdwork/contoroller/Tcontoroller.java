f356e985076524269243864f8e80a7294cd5622c
tomari09 <programbenkyoyou@gmail.com>
2021-12-17 19:33:50 +0900

    登録処理完了　（登録し続けるのみ）

--- src/main/java/curdwork/contoroller/Tcontoroller.java
+++ src/main/java/curdwork/contoroller/Tcontoroller.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import curdwork.entity.Tentity;
 import curdwork.service.Tservice;
@@ -28,13 +30,14 @@ public class Tcontoroller {
 	//登録画面に遷移する
 	@GetMapping(value = "/user/form")
 	  public String form(Model model) {
+		model.addAttribute("tentity", new Tentity());
 	    return "user/form";  
 	} 
 	//登録画面の登録を受け取る
-	/** @PostMapping("/user/form")
+	 @PostMapping("/user/form")
 	    public String insert(@ModelAttribute Tentity tentity) {
 		 tservice.insert(tentity);
 	        return "user/form";
-	    }*/
+	 }
 
 }
